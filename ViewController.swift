41884b7bfd54deb92dfef2575a54c07d5a7f4d8c
Sudang <80115601+suyeon100@users.noreply.github.com>
2022-02-24 17:46:29 +0900

    Todo first commit
    Login firebase use

---
+++ ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by 박수연 on 2022/02/23.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var todoLogo: UIImageView!
+    
+    
+    @IBOutlet weak var idText: UITextField!
+    
+    @IBOutlet weak var pwText: UITextField!
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if Auth.auth().currentUser != nil {
+
+                  idText.placeholder = "이미 로그인 된 상태입니다."
+
+                   pwText.placeholder = "이미 로그인 된 상태입니다."
+
+                   loginButton.setTitle("이미 로그인 된 상태입니다.", for: .normal)
+
+               }
+
+    }
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: idText.text!, password: pwText.text!) { (user, error) in
+
+                   if user != nil{
+
+                       print("login success")
+
+                   }
+
+                   else{
+
+                       print("login fail")
+        
+    }
+    
+}
+        guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "TabBarController") else { return }
+         self.navigationController?.pushViewController(homeVC, animated: true)
+        
+    }
+ 
+    
+}
